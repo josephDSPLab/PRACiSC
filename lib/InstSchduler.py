77aabd5d50ba4527355c13254761ccbde7906d14
josephDSPLab <josephDSPLab@gmail.com>
2018-06-01 17:01:30 +0800

    Add files via upload

---
+++ lib/InstSchduler.py
@@ -0,0 +1,26 @@
+'''
+Scheduler
+Used for the controling the music event on time
+
+'''
+import schedule
+import time
+
+class InstScheduler():
+    def __init__():
+        s = sched.scheduler(time.time, time.sleep)
+        Inst_dict = {}
+
+    def AddInst(self, Inst_name, Instcontrol):
+        '''
+        Automatically overwrite
+        '''
+        Inst_dict[Inst_name] = Instcontrol
+
+    def AddEvent(self, Inst_name, delay_beat):
+
+    def Eventpipeline():
+
+
+    def __getitem__(self, Inst_name):
+        return Inst_dict[Inst_name]
