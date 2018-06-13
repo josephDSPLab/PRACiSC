341d33dacc7f81dc2ac04da5b3b46c50fb9e2e8d
josephDSPLab <josephDSPLab@gmail.com>
2018-06-13 22:40:03 +0800

    Add live generating event mode
    Currently, it can randomly generate the play or stop event (in the next loop) online. It still need some rules to generate a better result.
    
    The loop_event add two new attribute: time_stamp and volatile

--- lib/InstSchduler.py
+++ lib/InstSchduler.py
@@ -10,9 +10,13 @@ import time
 import FoxDot
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+from scipy import signal
+
 from loop_event import EventPipeline
 from tool.midi2p import midi2P
 from tool.midi2tracks import TrackSet
+from tool.drawtool import draw_function
 
 
 class InstScheduler():
@@ -24,8 +28,9 @@ class InstScheduler():
         self.player_dict = {}
         self.parameters = {}
         self.playing_flag = {}
+        self.need_prepare = 0
         self.pipeline = EventPipeline()
-        self.pipeline.Processor(self.EventProcessor)
+        self.pipeline.Processor(self.EventProcessor, self.fclock_now)
 
     def __getitem__(self, Inst_name):
         return self.Inst_dict[Inst_name]
@@ -53,6 +58,9 @@ class InstScheduler():
             Inst_name](**self.parameters[Inst_name])
         self.playing_flag[Inst_name] = True
 
+    def fclock_now(self):
+        return self.fclock.now() / 16
+
     def AddMidi(self, Inst_name, port_num):
         Patterns = midi2P(file_path, None, verb=0)
         Patterns[0]['MidiNum'] = port_num
@@ -77,16 +85,13 @@ class InstScheduler():
 
     def EventProcessor(self, event_type, event_arg):
         '''
-        read Score from data -> do the correspond method
-        '''
-
-        '''
         Read-In and pipeline
         '''
 
         '''
         Action
         '''
+
         if hasattr(event_type, '__iter__') == False:
             event_type = [event_type]
         if hasattr(event_arg, '__iter__') == False:
@@ -96,60 +101,120 @@ class InstScheduler():
                 if arg['Inst_name'] not in self.Inst_dict:
                     self.AddInst(**arg)
                 self.PlayInst(Inst_name=arg['Inst_name'])
-            if e == 'Stop':
-                self.StopInst(**arg)
-            if e == 'ChangeParameter':
+            elif e == 'Stop':
+                self.StopInst(Inst_name=arg['Inst_name'])
+            elif e == 'ChangeParameter':
                 self.change_parameters(**arg)
+            elif e == 'Gen_Live':
+                self.live_generator(**arg)
+            else:
+                print('Event type Wrong!')
 
         return
 
     def Ordered_event(self):
-
-        for k in self.Inst_dict.keys():
-            self.pipeline.PullEvent(['Play'], [{'Inst_name': k}])
+        self.pipeline.event_clear()
+        for i, k in enumerate(self.Inst_dict.keys()):
+            self.pipeline.PushEvent(['Play'], [{'Inst_name': k}], i)
         return
+
+    def Random_event(self, period_bar):
+        self.pipeline.event_clear()
+        perm_key = np.random.permutation(list(self.Inst_dict.keys()))
+        for i, k in enumerate(perm_key):
+            self.pipeline.PushEvent(
+                ['Play'], [{'Inst_name': k}], i * period_bar)
+
+    def live_generator(self, prev_pros, pros, event_time):
+        '''
+        Real-time generating the instrument playing event.
+        need to perform with some rule to generate a better result
+        '''
+        delt_pros = pros - prev_pros
+
+        if delt_pros > 0:
+            inst_play = [key for key,
+                         value in self.playing_flag.items() if not value]
+            method = ['Play'] * int(delt_pros)
+            new_evet = np.random.choice(inst_play, int(delt_pros),replace=False)
+            args = [{'Inst_name': n} for n in new_evet]
+            print(method[0],args)
+            self.pipeline.InsertEvent(
+                method, args, event_time, vol=True)
+            return
+        if delt_pros < 0:
+            inst_play = [key for key,
+                         value in self.playing_flag.items() if value]
+            method = ['Stop'] * int(-delt_pros)
+            new_evet = np.random.choice(inst_play, int(-delt_pros),replace=False)
+            args = [{'Inst_name': n} for n in new_evet]
+            print(['Stop'],args)
+            self.pipeline.InsertEvent(
+                method, args, event_time, vol=True)
+            return
 
     def Live_event(self):
         '''
         Define a prosperity function which can limit the current number of playing Instruments
         **** Not implemented well Now ****
         '''
+        self.pipeline.event_clear()
+        self.need_prepare = 1
+        num_level = min(10,len(self.Inst_dict))
         num_point = 5
         min_pos = 0.2
         max_pos = 1
-        Pros_tri = np.linspace(min_pos, max_pos, num=(num_point - 1) / 2)
-        pros = np.concatenate([Pros_tri, Pros_tri.reverse()])
+        self.canvas_width = 500
+        self.canvas_height = 300
+        self.bd = 20
+        y_max = self.canvas_height - self.bd
+        y_min = self.bd
+        canvs = draw_function(self.canvas_width, self.canvas_height, self.bd)
+        raw_data = np.array(canvs.data).T
+        flip_y = y_max - raw_data[1]
 
-        for k in self.Inst_dict.keys():
-            self.pipeline.PullEvent()
+        quant_y = np.minimum(
+            np.round(flip_y / y_max * num_level + 2), num_level)
+        loop_y = np.concatenate([quant_y, np.flip(quant_y, 0)])
 
-    def Random_event(self):
-        perm_key = np.random.permutation(list(self.Inst_dict.keys()))
-        for k in perm_key:
-            print(k)
-            self.pipeline.PullEvent(['Play'], [{'Inst_name': k}])
+        # determine a proper period
+        self.period = num_level * 4
+
+        resample_y = np.round(signal.resample(loop_y, self.period))
+        '''
+        plt.plot(resample_y)
+        plt.title('prosperity function')
+        plt.show()
+        '''
+        prev_pros = 0
+        # generate live random event
+        for p, b in zip(resample_y, range(self.period)):
+            if p != prev_pros:
+                self.pipeline.PushEvent(
+                    ["Gen_Live"], [{'prev_pros': prev_pros, 'pros': p, 'event_time': b}], b - 1)
+            prev_pros = p
 
     def CheckingAsyLoop(self, beat_bar=4, bar_loop=4):
         '''
         Asynchronous main loop for checking the event online
         '''
         #self.play_bpm = self.fclock.bpm = play_bpm
-        # self.fclock.set_time(0)
-        self.pipeline.sent_request()
+        self.fclock.set_time(0.1)
+        cont = self.pipeline.sent_request()
         self.fclock.set_time(-0.5)
-        self.start_time = time.time()
-        print(self.start_time-self.sent_time)
-        loop_dur = beat_bar * bar_loop * (60. / self.fclock.bpm)
 
+        self.start_time = time.time()
+        loop_dur = beat_bar * bar_loop * (60. / self.fclock.bpm)
         sleep_time = loop_dur / 2.
+        while(cont):
+            cont = self.pipeline.sent_request(0)
         while(1):
-            time_wait = sleep_time - \
-                ((time.time() - self.start_time) % sleep_time)
-            time.sleep(time_wait)
-            
+
             time_take = loop_dur - ((time.time() - self.start_time) % loop_dur)
             self.s.enter(time_take, 1, self.pipeline.sent_request, (0,))
-            self.s.run()
+            cont = self.s.run()
+            while(cont):
+                cont = self.pipeline.sent_request(0)
 
         return
 
@@ -157,33 +222,51 @@ class InstScheduler():
         '''
         Start the event pipeline after delay_time directly
         '''
+        self.sent_time = time.time()
+        if self.need_prepare:
+            self.fclock.set_time(-16)
+            cont = self.pipeline.sent_request(0)
+            while(cont):
+                cont = self.pipeline.sent_request(0)
+        #self.start_time = time.time()
         self.fclock.set_time(-0.1)
         self.play_bpm = self.fclock.bpm = play_bpm
-        self.sent_time = time.time()
-        #self.start_time = time.time()
-        loop_thread = threading.Timer(delay_time, self.CheckingAsyLoop, ())
+        loop_thread = threading.Timer(
+            delay_time - 60. / play_bpm * 1, self.CheckingAsyLoop, ())
         loop_thread.daemon = True
         loop_thread.start()
+
+        loop_thread_test = threading.Timer(
+            delay_time, lambda: print(self.fclock_now()), ())
+        loop_thread_test.daemon = True
+        loop_thread_test.start()
         return
 
 
 if __name__ == '__main__':
 
-    folder_path = 'tool\\Midinote-Test2'
+    folder_path = 'tool\\test_midi_folder'
     '''
     test = InstScheduler(FoxDot.lib.Clock)
     test.AddMidiFolder(folder_path)
-    print(test)
     test.Ordered_event()
-    test.StartInTime(2, 140)
+    test.StartInTime(2, 160)
     '''
-    
+
+    #'''
     test2 = InstScheduler(FoxDot.lib.Clock)
     test2.AddMidiFolder(folder_path)
-    print(test2)
-    test2.Random_event()
-    test2.StartInTime(2, 140)
-    
+    test2.Random_event(2)
+    test2.StartInTime(2, 160)
+    #'''
+
+    '''
+    test3 = InstScheduler(FoxDot.lib.Clock)
+    test3.AddMidiFolder(folder_path)
+    test3.Live_event()
+    test3.StartInTime(2, 160)
+    '''
+
     while(1):
         print('outside loop')
         time.sleep(2)
--- lib/loop_event.py
+++ lib/loop_event.py
@@ -5,31 +5,58 @@ registers int
 
 '''
 import collections
+import numpy as np
 
 class EventPipeline:
     def __init__(self):
+        self.event_clear()
+
+        # processor init
+        self._proc = None
+        self._timer = None
+
+    def event_clear(self):
         self.event_list = []
         self.eventcount = 0
         self.processed_count = 0
         self.running_event = None
         self.next_event = None
-        self._proc = None
         self.running_done = True
-    def PullEvent(self, method_name, args):
+        
+    def PushEvent(self, method_name, args, time_stamp, vol=False):
+        '''
+        time_stamp: integer type, when to do the process
+        '''
         if not  isinstance(method_name, collections.Iterable):
             method_name=[method_name]
         if not  isinstance(args, collections.Iterable):
             method_name=[args]
-        self.event_list.append([method_name, args])
+        event = {'method': method_name,'args':args,'time_stamp':time_stamp,'volatile':vol}
+        self.event_list.append(event)
         self.eventcount += 1
         if self.next_event is None:
-            self.next_event = [method_name, args]
+            self.next_event = self.event_list[-1]
 
-    def Processor(self, func):
+    def InsertEvent(self, method_name, args, time_stamp, vol=False, loc = 'head'):
+        if loc == 'head':
+            idx = next((i for i,e in enumerate(self.event_list) if int(e['time_stamp']) >= int(time_stamp)), self.eventcount)
+        if loc == 'tail':
+            idx = next(i for i,e in enumerate(self.event_list) if e['time_stamp'] >= time_stamp)
+        event = {'method': method_name,'args':args,'time_stamp':time_stamp,'volatile':vol}
+        self.event_list.insert(idx,event)
+        self.eventcount += 1
+        if self.next_event is None:
+            self.next_event = self.event_list[-1]
+        if self.next_event['time_stamp'] >= time_stamp:
+            self.next_event = self.event_list[idx]
+
+
+    def Processor(self, func, timer):
         '''
         func should be a callable object with knowing method_nam and corresponding args
         '''
-        self._proc = func 
+        self._proc = func
+        self._timer = timer 
         return
 
     def sent_request(self,verb=0):
@@ -38,13 +65,24 @@ class EventPipeline:
         '''
         if verb > 0:
             print(self.next_event)
-        #for method, args in zip(*self.next_event):
-        #    print(method)
-        #    print(args)
-        self._proc(self.next_event[0], self.next_event[1])
-        self.processed_count += 1
-        if self.processed_count < len(self.event_list):
+
+        if np.round(self._timer() - self.next_event['time_stamp']) >= 0:
+            self._proc(self.next_event['method'], self.next_event['args'])
+            self.processed_count += 1
+        else:
+            return 0
+        if self.processed_count < self.eventcount:
             self.next_event = self.event_list[self.processed_count]
         else:
             self.processed_count = 0
             self.next_event = self.event_list[0]
+
+        if self.event_list[self.processed_count - 1]['volatile']:
+            del self.event_list[self.processed_count-1]
+            self.eventcount -= 1
+            self.processed_count -= 1
+
+        if self.next_event['time_stamp'] == self.event_list[self.processed_count-1]['time_stamp']:    
+            return 1
+        else:
+            return 0
